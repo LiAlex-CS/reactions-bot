58a1be3b1d6fbfcca1ad8c7f7fc004f660282a2f
Alex Li <lialex5883@gmail.com>
2023-07-15 01:58:09 -0400

    added reactions in js

---
+++ dist/commands/reaction_command_details.js
@@ -0,0 +1,426 @@
+export const ReactionCommands = {
+    'caught-in-4k': {
+        name: 'caught-in-4k',
+        description: 'Caught In 4K Text',
+        reactions: ['🇨', '🇦', '🇺', '🇬', '🇭', '🇹', '🇮', '🇳', '4️⃣', '🇰', '📸']
+    },
+    nerd: {
+        name: 'nerd',
+        description: 'Nerd Text',
+        reactions: ['🇳', '🇪', '🇷', '🇩', '☝️', '🤓']
+    },
+    whodat: {
+        name: 'whodat',
+        description: 'Whodat? Text',
+        reactions: ['🇼', '🇭', '🇴', '🇩', '🇦', '🇹', '❓']
+    },
+    'who-are-you': {
+        name: 'who-are-you',
+        description: 'Who Are You? Text',
+        reactions: ['🇼', '🇭', '🇴', '🇦', '🇷', '🇪', '🇾', '🇴', '🇺', '❓']
+    },
+    'we-up': {
+        name: 'we-up',
+        description: 'We Up Text',
+        reactions: ['🇼', '🇪', '🆙']
+    },
+    'we-on-top': {
+        name: 'we-on-top',
+        description: 'We On Top Text',
+        reactions: ['🇼', '🇪', '🔛', '🔝']
+    },
+    WM: {
+        name: 'WM',
+        description: 'Woman Moment Text',
+        reactions: ['🇼', '🇲', '♀️']
+    },
+    MM: {
+        name: 'MM',
+        description: 'Man Moment Text',
+        reactions: ['🇲', '🇲', '♂️']
+    },
+    OK: {
+        name: 'OK',
+        description: 'OK Text',
+        reactions: ['🆗', '👌']
+    },
+    neurodivergent: {
+        name: 'neurodivergent',
+        description: 'neurodivergent Text',
+        reactions: ['🇳', '🇪', '🇺', '🇷', '🇴', '🇩', '🇮', '🇻', '🚫', '🧠']
+    },
+    neurotypical: {
+        name: 'neurotypical',
+        description: 'neurotypical Text',
+        reactions: [
+            '🇳',
+            '🇪',
+            '🇺',
+            '🇷',
+            '🇴',
+            '🇹',
+            '🇾',
+            '🇵',
+            '🇮',
+            '🇨',
+            '🇦',
+            '🇱',
+            '🧠'
+        ]
+    },
+    'lets-go': {
+        name: 'lets-go',
+        description: 'Lets Go Text',
+        reactions: ['🇱', '🇪', '🇹', '🇸', '🇬', '🇴', '‼️']
+    },
+    'real-shit': {
+        name: 'real-shit',
+        description: 'Real Shit Text',
+        reactions: ['🇷', '🇪', '🇦', '🇱', '🇸', '🇭', '🇮', '🇹', '‼️']
+    },
+    'for-real': {
+        name: 'for-real',
+        description: 'For Real? Text',
+        reactions: ['🇫', '🇴', '🇷', '🇪', '🇦', '🇱', '❓']
+    },
+    'on-god': {
+        name: 'on-god',
+        description: 'On God Text',
+        reactions: ['🇴', '🇳', '🇬', '🅾️', '🇩', '✝️']
+    },
+    'light-work': {
+        name: 'light-work',
+        description: 'Light Work Text',
+        reactions: ['🇱', '🇮', '🇬', '🇭', '🇹', '🇼', '🇴', '🇷', '🇰']
+    },
+    'big-body': {
+        name: 'big-body',
+        description: 'Big Body Text',
+        reactions: ['🇧', '🇮', '🇬', '🇧', '🇴', '🇩', '🇾']
+    },
+    gyat: {
+        name: 'gyat',
+        description: 'Gyat Text',
+        reactions: ['🇬', '🇾', '🇦', '🇹', '🍑']
+    },
+    nut: {
+        name: 'nut',
+        description: 'Nut Text',
+        reactions: ['🇳', '🇺', '🇹', '🥜']
+    },
+    autism: {
+        name: 'autism',
+        description: 'Autism Text',
+        reactions: ['🇦', '🇺', '🇹', '🇮', '🇸', '🇲']
+    },
+    king: {
+        name: 'king',
+        description: 'King Text',
+        reactions: ['🇰', '🇮', '🇳', '🇬', '👑']
+    },
+    bet: {
+        name: 'bet',
+        description: 'Bet Text',
+        reactions: ['🇧', '🇪', '🇹', '🤝']
+    },
+    broski: {
+        name: 'broski',
+        description: 'Broski Text',
+        reactions: ['🇧', '🇷', '🇴', '🇸', '🇰', '🇮']
+    },
+    damn: {
+        name: 'damn',
+        description: 'Damn Text',
+        reactions: ['🇩', '🇦', '🇲', '🇳']
+    },
+    'i-dont-care': {
+        name: 'i-dont-care',
+        description: "I Don't Care Text",
+        reactions: ['🇮', '🇩', '🇴', '🇳', '🇹', '🇨', '🇦', '🇷', '🇪']
+    },
+    goodbye: {
+        name: 'goodbye',
+        description: 'Goodbye Text',
+        reactions: ['🇬', '🇴', '🅾️', '🇩', '🇧', '🇾', '🇪']
+    },
+    'walk-faster': {
+        name: 'walk-faster',
+        description: 'Walk Faster Text',
+        reactions: ['🇼', '🇦', '🇱', '🇰', '🇫', '🅰️', '🇸', '🇹', '🇪', '🇷']
+    },
+    stinky: {
+        name: 'stinky',
+        description: 'Stinky Text',
+        reactions: ['🇸', '🇹', '🇮', '🇳', '🇰', '🇾', '🤢']
+    },
+    ew: {
+        name: 'ew',
+        description: 'Ew Text',
+        reactions: ['🇪', '🇼', '🤢']
+    },
+    'gone-atm': {
+        name: 'gone-atm',
+        description: 'Gone At The Moment Text',
+        reactions: ['🇬', '🇴', '🇳', '🇪', '🏧']
+    },
+    immigrant: {
+        name: 'immigrant',
+        description: 'Gone At The Moment Text',
+        reactions: ['🇮', '🇲', 'Ⓜ️', 'ℹ️', '🇬', '🇷', '🇦', '🇳', '🇹']
+    },
+    valorant: {
+        name: 'valorant',
+        description: 'Valorant? Text',
+        reactions: ['🇻', '🇦', '🇱', '🇴', '🇷', '🇦', '🇳', '🇹', '❓']
+    },
+    apex: {
+        name: 'apex',
+        description: 'Apex? Text',
+        reactions: ['🇦', '🇵', '🇪', '🇽', '❓']
+    },
+    minecraft: {
+        name: 'minecraft',
+        description: 'Minecraft? Text',
+        reactions: ['🇦', '🇵', '🇪', '🇽', '❓']
+    },
+    'bad-joke': {
+        name: 'bad-joke',
+        description: 'Bad Joke Text',
+        reactions: ['🇧', '🇦', '🇩', '🇯', '🇴', '🇰', '🇪', '😡']
+    },
+    pluh: {
+        name: 'pluh',
+        description: 'Pluh Text',
+        reactions: ['🇵', '🇱', '🇺', '🇭', '📣', '‼️']
+    },
+    'dumb-bitch': {
+        name: 'dumb-bitch',
+        description: 'Dumb Bitch Text',
+        reactions: ['🇩', '🇺', '🇲', '🇧', '🅱️', '🇮', '🇹', '🇨', '🇭']
+    },
+    bitch: {
+        name: 'bitch',
+        description: 'Bitch Text',
+        reactions: ['🇧', '🇮', '🇹', '🇨', '🇭']
+    },
+    'punk-bitch': {
+        name: 'punk-bitch',
+        description: 'Punk Bitch Text',
+        reactions: ['🇵', '🇺', '🇳', '🇰', '🇧', '🇮', '🇹', '🇨', '🇭']
+    },
+    monkey: {
+        name: 'monkey',
+        description: 'Monkey Text',
+        reactions: ['🇲', '🇴', '🇳', '🇰', '🇪', '🇾', '🐵']
+    },
+    bingbong: {
+        name: 'bingbong',
+        description: 'Bingbong Text',
+        reactions: ['🇧', '🇮', '🆖', '🅱️', '🇴', '🇳', '🇬']
+    },
+    tekwon: {
+        name: 'tekwon',
+        description: 'Tekwon72 Text',
+        reactions: ['🇹', '🇪', '🇰', '🇼', '🇴', '🇳', '7️⃣', '2️⃣']
+    },
+    breadstick: {
+        name: 'breadstick',
+        description: 'Breadstick Text',
+        reactions: ['🇧', '🇷', '🇪', '🇦', '🇩', '🇸', '🇹', '🇮', '🇨', '🇰']
+    },
+    bagel: {
+        name: 'bagel',
+        description: 'Bagel Text',
+        reactions: ['🇧', '🇦', '🇬', '🇪', '🇱']
+    },
+    'up-shut': {
+        name: 'up-shut',
+        description: 'Up Shut Text',
+        reactions: ['🆙', '🇸', '🇭', '🇺', '🇹']
+    },
+    'shut-up': {
+        name: 'shut-up',
+        description: 'Shut Up Text',
+        reactions: ['🇸', '🇭', '🇺', '🇹', '🆙']
+    },
+    parvis: {
+        name: 'parvis',
+        description: 'Parvis Text',
+        reactions: ['🇵', '🇦', '🇷', '🇻', '🇮', '🇸']
+    },
+    dwbaek: {
+        name: 'dwbaek',
+        description: 'Dwbaek Text',
+        reactions: ['🇩', '🇼', '🇧', '🇦', '🇪', '🇰']
+    },
+    yurida: {
+        name: 'yurida',
+        description: 'Yurida Text',
+        reactions: ['🇾', '🇺', '🇷', '🇮', '🇩', '🇦']
+    },
+    jangsta: {
+        name: 'jangsta',
+        description: 'Jangsta Text',
+        reactions: ['🇯', '🇦', '🇳', '🇬', '🇸', '🇹', '🅰️']
+    },
+    efrt: {
+        name: 'efrt',
+        description: 'EFRT Text',
+        reactions: ['🇪', '🇫', '🇷', '🇹']
+    },
+    ayush: {
+        name: 'ayush',
+        description: 'Ayush Text',
+        reactions: ['🇦', '🇾', '🇺', '🇸', '🇭']
+    },
+    jason: {
+        name: 'jason',
+        description: 'Jason Text',
+        reactions: ['🇯', '🇦', '🇸', '🇴', '🇳']
+    },
+    lisa: {
+        name: 'lisa',
+        description: 'Lisa Text',
+        reactions: ['🇱', '🇮', '🇸', '🇦']
+    },
+    david: {
+        name: 'david',
+        description: 'David Text',
+        reactions: ['🇩', '🇦', '🇻', '🇮', '🇩']
+    },
+    shawty: {
+        name: 'shawty',
+        description: 'Shawty Text',
+        reactions: ['🇸', '🇭', '🇦', '🇼', '🇹', '🇾', '🫦']
+    },
+    yer: {
+        name: 'yer',
+        description: 'Yer Text',
+        reactions: ['🇾', '🇪', '🇷', '🔥']
+    },
+    'im-gay': {
+        name: 'im-gay',
+        description: 'Im Gay Text',
+        reactions: ['🇮', '🇲', '🇬', '🇦', '🇾', '🏳️‍🌈']
+    },
+    goober: {
+        name: 'goober',
+        description: 'Goober Text',
+        reactions: ['🇬', '🇴', '🅾️', '🇧', '🇪', '🇷']
+    },
+    china: {
+        name: 'china',
+        description: 'China Text',
+        reactions: ['🇨', '🇭', '🇮', '🇳', '🇦', '🇨🇳']
+    },
+    korean: {
+        name: 'korean',
+        description: 'Korean Text',
+        reactions: ['🇰', '🇴', '🇷', '🇪', '🇦', '🇳', '🇰🇷']
+    },
+    africa: {
+        name: 'africa',
+        description: 'Africa Text',
+        reactions: ['🇦', '🇫', '🇷', '🇮', '🇨', '🅰️']
+    },
+    'joe-biden': {
+        name: 'joe-biden',
+        description: 'Joe Biden Text',
+        reactions: ['🇯', '🇴', '🇪', '🇧', '🇮', '🇩', '🇪', '🇳', '👴']
+    },
+    heat: {
+        name: 'heat',
+        description: 'Heat Text',
+        reactions: ['🇭', '🇪', '🇦', '🇹', '🔥']
+    },
+    fire: {
+        name: 'fire',
+        description: 'Fire Text',
+        reactions: ['🇫', '🇮', '🇷', '🇪', '🔥']
+    },
+    'crank-me-up': {
+        name: 'crank-me-up',
+        description: 'Crank Me Up Text',
+        reactions: ['🇨', '🇷', '🇦', '🇳', '🇰', '🇲', '🇪', '🇺', '🇵', '💯']
+    },
+    'free-smoke': {
+        name: 'free-smoke',
+        description: 'Free Smoke Text',
+        reactions: ['🆓', '🇸', '🇲', '🇴', '🇰', '🇪']
+    },
+    'cool-shit-bro': {
+        name: 'cool-shit-bro',
+        description: 'Cool Shit Bro Text',
+        reactions: ['🆒', '🇸', '🇭', '🇮', '🇹', '🇧', '🇷', '🇴']
+    },
+    'very-cool': {
+        name: 'very-cool',
+        description: 'Very Cool Text',
+        reactions: ['🇻', '🇪', '🇷', '🇾', '🆒', '😎']
+    },
+    'good-shit': {
+        name: 'good-shit',
+        description: 'Good Shit Text',
+        reactions: ['🇬', '🇴', '🅾️', '🇩', '🇸', '🇭', '🇮', '🇹']
+    },
+    wp: {
+        name: 'wp',
+        description: 'Well Played Text',
+        reactions: ['🇼', '🇵', '👏']
+    },
+    bruh: {
+        name: 'bruh',
+        description: 'Bruh Text',
+        reactions: ['🇧', '🇷', '🇺', '🇭']
+    },
+    what: {
+        name: 'what',
+        description: 'What Text',
+        reactions: ['🇼', '🇭', '🇦', '🇹', '💯', '🔥', '📣', '‼️']
+    },
+    snake: {
+        name: 'snake',
+        description: 'Snake Text',
+        reactions: ['🇸', '🇳', '🇦', '🇰', '🇪', '🐍']
+    },
+    'too-many-ops': {
+        name: 'too-many-ops',
+        description: 'Too Many Ops Text',
+        reactions: ['🇹', '🇴', '🅾️', '🇲', '🇦', '🇳', '🇾', '⭕', '🇵', '🇸', '😤']
+    },
+    westbrick: {
+        name: 'westbrick',
+        description: 'Westbrick Text',
+        reactions: ['🇼', '🇪', '🇸', '🇹', '🇧', '🇷', '🇮', '🇨', '🇰', '🧱']
+    },
+    lebron: {
+        name: 'lebron',
+        description: 'Lebron Text',
+        reactions: ['🇱', '🇪', '🇧', '🇷', '🇴', '🇳', '🏀']
+    },
+    kobe: {
+        name: 'kobe',
+        description: 'Kobe Text',
+        reactions: ['🇰', '🇴', '🇧', '🇪', '🏀']
+    },
+    sauce: {
+        name: 'sauce',
+        description: 'Sauce Text',
+        reactions: ['🇸', '🇦', '🇺', '🇨', '🇪']
+    },
+    zesty: {
+        name: 'zesty',
+        description: 'Zesty Text',
+        reactions: ['🇿', '🇪', '🇸', '🇹', '🇾', '💅']
+    },
+    peace: {
+        name: 'peace',
+        description: 'Peace Text',
+        reactions: ['🇵', '🇪', '🇦', '🇨', '🇪', '✌️']
+    },
+    'i-love-u': {
+        name: 'i-love-u',
+        description: 'I Love U Text',
+        reactions: ['🇮', '🇱', '🇴', '🇻', '🇪', '🇺', '♥️']
+    }
+};
